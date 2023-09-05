4f8e4bca193e508891aced5bdd0602b23cd482b6
purgz <henbrooks6@gmail.com>
2023-09-05 15:31:08 +0100

    added configuration for env.properties with api credentials

--- src/main/java/com/hbrooks/Application.java
+++ src/main/java/com/hbrooks/Application.java
@@ -1,13 +1,33 @@
 package com.hbrooks;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
+	@Value("${REAL_TIME_TRAIN_USERNAME}")
+	private String name;
+	@Value("${REAL_TIME_TRAIN_PASSWORD}")
+	private String password;
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
+
 	}
+
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+
+			System.out.println(name);
+			System.out.println(password);
+		};
+	}
+
 
 }
