c5d45e8bba54bc78cedc86d41f882baffd7828c2
purgz <henbrooks6@gmail.com>
2023-09-19 16:02:51 +0100

    updated login controller to return a loginDto as response

--- src/main/java/com/hbrooks/login/LoginController.java
+++ src/main/java/com/hbrooks/login/LoginController.java
@@ -1,11 +1,14 @@
 package com.hbrooks.login;
 
+import com.hbrooks.user.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +24,24 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto){
+    public ResponseEntity<LoginResponse> authenticateUser(@RequestBody LoginDto loginDto){
+
 
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 loginDto.getUsername(), loginDto.getPassword()
         ));
 
-        return new ResponseEntity<>("User login successfully", HttpStatus.OK);
+        System.out.println(authentication.getName());
+        System.out.println(authentication.getAuthorities());
+        System.out.println(authentication.getPrincipal());
+
+        LoginResponse loginResponse = new LoginResponse(
+                authentication.getName(),
+                loginDto.getPassword(),
+                authentication.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return new ResponseEntity<>(loginResponse, HttpStatus.OK);
     }
 }
